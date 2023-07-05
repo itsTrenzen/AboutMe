221f491b8198cfcfbb324236f5fb2f610ae0f7fe
Trenzen <103641280+itsTrenzen@users.noreply.github.com>
2023-07-06 00:50:55 +0200

    Added 2nd project

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ function playSnake() {
     //window.open("http://sn4ke.epizy.com/");
     window.open("/Snake-game/snake.html");
 }
+function playTic() {
+    window.open("/TicTacToe/TicTacToe.html");
+}
