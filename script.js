f2407f6228af838448f4de9909dfbdd0d11cc6b6
Trenzen <103641280+itsTrenzen@users.noreply.github.com>
2023-08-30 00:10:20 +0200

    Created first files

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+function redirect(lang) {
+    if (lang === "de") window.open("./IndexDE.html");
+    if (lang === "en") window.open("./IndexEN.html");
+}
 function playSnake() {
     //window.open("http://sn4ke.epizy.com/");
     window.open("/Snake-game/snake.html");
